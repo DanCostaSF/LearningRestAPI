259cbe920b3b2741147a6b60e25344a9d6a13181
Danilo Costa <dancostasf@gmail.com>
2022-12-07 16:49:05 -0300

    delete unused export

--- app/src/main/java/com/ocanha/retrofitcomkotlin/data/di/NetworkModules.kt
+++ app/src/main/java/com/ocanha/retrofitcomkotlin/data/di/NetworkModules.kt
@@ -1,14 +1,9 @@
 package com.ocanha.retrofitcomkotlin.data.di
 
-import com.google.gson.GsonBuilder
 import com.ocanha.retrofitcomkotlin.commons.NullOnEmptyConverterFactory
 import com.ocanha.retrofitcomkotlin.data.network.RecipeService
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object NetworkModules {
 
@@ -22,19 +17,4 @@ object NetworkModules {
             .build()
             .create(RecipeService::class.java)
     }
-
-//    fun recipeService() : RecipeService = OkHttpClient.Builder().addNetworkInterceptor { chain ->
-//        val request = chain.request()
-//        val response: okhttp3.Response = chain.proceed(request)
-//        if(response.code() == 204) response.newBuilder().code(200).build()
-//        else response
-//    }.build().run {
-//        Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(NullOnEmptyConverterFactory())
-//            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-//            .client(this)
-//            .build()
-//            .create(RecipeService::class.java)
-//    }
 }
